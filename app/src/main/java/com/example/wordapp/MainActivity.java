7cfd9f8b8490dd68752abf16eafe3fb1ed94056c
prajolitabista <prajolita.bista@gmail.com>
2023-03-18 14:19:27 +0545

    Initial commit

---
+++ app/src/main/java/com/example/wordapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.wordapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
