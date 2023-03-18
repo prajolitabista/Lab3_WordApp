d2aa1f5dd82070d9943b63c9c765c49eebcc9dca
prajolitabista <prajolita.bista@gmail.com>
2023-03-18 14:38:35 +0545

    Word app commits

---
+++ app/src/main/java/com/example/wordapp/LetterModel.java
@@ -0,0 +1,9 @@
+package com.example.wordapp;
+
+public class LetterModel {
+    String letter;
+
+    public LetterModel(String letter) {
+        this.letter = letter;
+    }
+}
---
+++ app/src/main/java/com/example/wordapp/WordModel.java
@@ -0,0 +1,9 @@
+package com.example.wordapp;
+
+public class WordModel {
+    String word;
+
+    public WordModel(String word) {
+        this.word = word;
+    }
+}
